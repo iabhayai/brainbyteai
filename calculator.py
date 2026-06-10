19c3c2176480994989a0a63c1940bc0d2507b69b
iabhayai <abhaykumart543@gmail.com>
2026-06-10 19:59:05 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,38 +1,44 @@
 while True:
+    try:
+        num1 = float(input("Enter your first number: "))
+        valid_operation = ["+", "-", "*", "**", "/", "%", "//" ]
+        while True:
+            operation = input("Enter your operator: ")
+            if operation in valid_operation:
+                break
+            else:
+                print("please enter valid operation")
+        num2 = float(input("Enter your second number: "))
 
-    num1 = float(input("Enter your first number: "))
-    operation = input("Enter your operator: ")
-    num2 = float(input("Enter your second number: "))
+        if operation == "+":
+            print(num1 + num2)
 
-    if operation == "+":
-        print(num1 + num2)
+        elif operation == "-":
+            print(num1 - num2)
 
-    elif operation == "-":
-        print(num1 - num2)
+        elif operation == "*":
+            print(num1 * num2)
 
-    elif operation == "*":
-        print(num1 * num2)
-        
-    elif operation == "**":
-        print(num1 ** num2)
+        elif operation == "**":
+            print(num1 ** num2)
 
-    elif operation == "/":
-        if num2 == 0:
-            print("Cannot divide by zero")
-        else:
+        elif operation == "/":
             print(num1 / num2)
-     
-    elif operation == "%":
-         print(num1%num2)
-         
-    elif operation == "//":
-         print(num1//num2)
 
-    else:
-        print("Invalid operator")
+        elif operation == "%":
+            print(num1 % num2)
 
-    choice = input("Do you want to continue? (yes/no): ")
+        elif operation == "//":
+            print(num1 // num2)
 
-    if choice.lower() == "no":
-        print("Calculator closed")
-        break
+        choice = input("Do you want to continue? (yes/no): ")
+
+        if choice.lower() == "no":
+            print("Calculator closed")
+            break
+
+    except ValueError:
+        print("please enter valid numbers")
+
+    except ZeroDivisionError:
+        print("Cannot divide by zero")
