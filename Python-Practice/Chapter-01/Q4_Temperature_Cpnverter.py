0f2c4e7827e6c5628ab4588de8f3b78912f46a55
iabhayai <abhaykumart543@gmail.com>
2026-07-15 13:07:56 +0530

    Add Q4 Temperature Converter program

---
+++ Python-Practice/Chapter-01/Q4_Temperature_Cpnverter.py
@@ -0,0 +1,8 @@
+# Chapter 1 - Question 4
+
+temp_celsius = float(input("Enter Temperature: "))
+print(f"Temperature in Celsius is {temp_celsius}")
+
+temp_fahrenheit = (temp_celsius * 9 / 5) + 32
+
+print(f"Temperature in Fahrenheit is {temp_fahrenheit:.2f}")
