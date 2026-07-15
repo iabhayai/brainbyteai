4b7137f52a31fa9495b2f20bbed1909df3717295
iabhayai <abhaykumart543@gmail.com>
2026-07-15 13:00:22 +0530

    Add Q1 User Input Program

---
+++ Python-Practice/Chapter-01/Q1_User_Input.py
@@ -0,0 +1,7 @@
+# Chapter 1 - Question 1
+# Take a user's name and age as input and print their age after 5 years
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hi {name}, in 5 years you'll be {age + 5}")
