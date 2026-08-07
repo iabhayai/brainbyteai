8c13153c3965d7f1e5b23c7fdbf05724e8d9babd
iabhayai <abhaykumart543@gmail.com>
2026-08-07 14:05:00 +0530

    Add program to display day name using if-elif-else

---
+++ Python-Practice/Chapter-04/Q3_day_name_using_numbers.py
@@ -0,0 +1,18 @@
+days = int(input("Please Enter a number in the range [1:7] to get the day: "))
+
+if days == 1:
+    print("Day 1 is Monday")
+elif days == 2:
+    print("Day 2 is Tuesday")
+elif days == 3:
+    print("Day 3 is Wednesday")
+elif days == 4:
+    print("Day 4 is Thursday")
+elif days == 5:
+    print("Day 5 is Friday")
+elif days == 6:
+    print("Day 6 is Saturday")
+elif days == 7:
+    print("Day 7 is Sunday")
+else:
+    print("Please enter a valid number in the range [1:7]")
